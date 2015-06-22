700189263b67bbaca0de69c376f83d9bb4d8da6e
Raimondo De Rose <raimondo_derose@yahoo.com>
2015-06-22 17:33:18 +0200

    c

--- BotTesterTests/BotTesterTests.m
+++ BotTesterTests/BotTesterTests.m
@@ -29,6 +29,17 @@
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
 
+- (void)testExample2 {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testExample3 {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTAssert(@"trre");
+}
+
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
